79abe8493bd8e94e2090e69bed0ba506cbe230f2
rayhankimi <r4yhankimi@gmail.com>
2024-12-29 15:59:47 +0800

    Fix fetch

--- server/api/latest-value.ts
+++ server/api/latest-value.ts
@@ -1,17 +1,5 @@
 export default defineEventHandler(async (event) => {
-    const config = useRuntimeConfig();
-    const apiSecret = config.apiSecret;
-
-    if (!apiSecret) {
-        throw createError({
-            statusCode: 500,
-            message: 'API_SECRET is missing in the runtime config',
-        });
-    }
-
-    return await $fetch('https://your-backend-api.example.com/data', {
-        headers: {
-            Authorization: `Token ${apiSecret}`,
-        },
+    return await $fetch('/api/user/device/1/latest-value', {
+        baseURL: useRuntimeConfig().public.baseURL,
     });
 });
